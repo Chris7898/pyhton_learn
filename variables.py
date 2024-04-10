84c2a85f304b94e665801276cedc0aaf10805cde
Chris B <115883974+Darkwolf7898@users.noreply.github.com>
2024-04-10 16:29:20 -0400

    type definer

--- variables.py
+++ variables.py
@@ -1,4 +1,14 @@
-x = "Chris"
-y = "Made"
-z = "This."
-print(x, y, z)
+
+# x = "Chris"
+# y = "Made"
+# z = "This."
+# print(x, y, z)
+
+
+x = 1
+y = 2.8
+z = 1j
+
+print(type(x))
+print(type(y))
+print(type(z))
